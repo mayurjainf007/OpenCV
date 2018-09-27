8a982e87eee4d12d3192090402974a02aff3412b
Mayur Jain <mayurjain333@gmail.com>
2018-09-27 17:46:32 +0530

    Add files via upload

---
+++ p1(Intro and loading Images).py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+img = cv2.imread('watch.jpg',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.imshow(img,cmap='gray',interpolation='bicubic')
+plt.plot([50,100],[100,80],'c',linewidth=5)
+plt.show()
+
+cv2.imwrite('watchgray.png',img)
+
+##plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+##plt.plot([200,300,400],[100,200,300],'c', linewidth=5)
+##plt.show()
+
